aad5a6c863bfbcde6fd149602a4f1bc7012722bf
noahsbored <permannoach@gmail.com>
2024-05-01 15:46:15 -0400

    yoooo

---
+++ moods.py
@@ -0,0 +1,8 @@
+def mood_response(mood):
+    if mood == 'happy':
+        return "nice!"
+    elif mood == 'sad':
+        return "so sorry to hear"
+    elif mood == 'okay':
+        return "awesome"
+   
---
+++ moods2.py
@@ -0,0 +1,4 @@
+import moods
+
+mood = input("How are you feeling today? ")
+print(moods.mood_response(mood))
